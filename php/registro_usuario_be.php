f60ba507af68d4de658ae08ce422e818698d0caf
Carlos Galindo <carlosgalindote4@gmail.com>
2024-06-08 09:42:30 -0400

    Validacion de usuario y correo

--- php/registro_usuario_be.php
+++ php/registro_usuario_be.php
@@ -8,6 +8,32 @@
 
     $query = "INSERT INTO users(username, email, password) VALUES('$username', '$email', '$password')";
 
+    //Verificar que el correo no se repita en la base de datos
+    $verificar_correo = mysqli_query($conexion, "SELECT * FROM users WHERE email='$email' ");
+
+    if(mysqli_num_rows($verificar_correo) > 0){
+        echo '
+            <script>
+                alert("Este correo ya está registrado, intenta con otro diferente");
+                window.location = "../index.php";
+            </script>
+        ';
+        exit();
+    }
+
+    //Verificar que el nombre de usuario no se repita en la DB
+    $verificar_usuario = mysqli_query($conexion, "SELECT * FROM users WHERE username='$username' ");
+
+    if(mysqli_num_rows($verificar_usuario) > 0){
+        echo '
+            <script>
+                alert("Este usuario ya está registrado, intenta con otro diferente");
+                window.location = "../index.php";
+            </script>
+        ';
+        exit();
+    }
+
     $ejecutar = mysqli_query($conexion, $query);
 
     if($ejecutar){
