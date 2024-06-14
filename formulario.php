99abaab0fa400946e9da1ad86ad391dde3883ef5
Carlos Galindo <carlosgalindote4@gmail.com>
2024-06-14 14:53:12 -0400

    Scroll de pestaña formulario

---
+++ formulario.php
@@ -0,0 +1,40 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="assets/styles/formulario.css">
+</head>
+<body>
+    <header>
+        <h2 class="logo">
+            <img src="assets/images/imagen-foto-removebg-preview.png" alt="Logo" class="logo-img">
+        </h2>
+        <nav class="navigation">
+            <a href="#">Home</a>
+            <a href="#">Service</a>
+            <a href="#">Nosotros</a>
+            <a href="#">Contacto</a>
+            <button class="btnLogin-popup">Login</button>
+        </nav>
+    </header>
+    <section class="zona1"></section>
+    <section class="zona2"></section>
+
+    <script type="text/javascript">
+        window.addEventListener("scroll", function(){
+            var header = document.querySelector("header");
+            header.classList.toggle("abajo",window.scrollY>0)
+        })
+    </script>
+
+    <script src="assets/script/scripts.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
